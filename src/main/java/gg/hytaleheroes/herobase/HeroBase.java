9c3c88458d3eac857db8465c646b198d58bc2023
tomalbrc <tomalbrc@me.com>
2026-01-29 00:22:05 +0100

    cleanup

--- src/main/java/gg/hytaleheroes/herobase/HeroBase.java
+++ src/main/java/gg/hytaleheroes/herobase/HeroBase.java
@@ -1,22 +1,19 @@
 package gg.hytaleheroes.herobase;
 
-import com.hypixel.hytale.server.core.event.events.ShutdownEvent;
 import com.hypixel.hytale.server.core.event.events.player.PlayerConnectEvent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import com.hypixel.hytale.server.core.universe.Universe;
-import com.hypixel.hytale.server.core.universe.world.events.ecs.ChunkSaveEvent;
 import com.hypixel.hytale.server.core.util.Config;
 import gg.hytaleheroes.herobase.command.BaseCommand;
 import gg.hytaleheroes.herobase.config.ModConfig;
-import gg.hytaleheroes.herobase.file.PlayerFirstJoinTrackerFile;
 import gg.hytaleheroes.herobase.format.TinyMsg;
 
 import javax.annotation.Nonnull;
+import java.io.IOException;
 
 public class HeroBase extends JavaPlugin {
     private final Config<ModConfig> config;
-    public static PlayerFirstJoinTrackerFile TRACKER = new PlayerFirstJoinTrackerFile();
 
     public HeroBase(@Nonnull JavaPluginInit init) {
         super(init);
@@ -28,19 +25,17 @@ public class HeroBase extends JavaPlugin {
         super.setup();
 
         this.config.load().thenAccept(x -> config.save()).join();
-        TRACKER.syncLoad();
 
         this.getCommandRegistry().registerCommand(new BaseCommand());
 
-        this.getEventRegistry().register(ShutdownEvent.class, (event) -> {
-            TRACKER.syncSave();
-        });
-        this.getEventRegistry().register(ChunkSaveEvent.class, (event) -> {
-            TRACKER.syncSave();
-        });
-
         this.getEventRegistry().register(PlayerConnectEvent.class, (event) -> {
-            boolean isNewPlayer = TRACKER.add(event.getPlayerRef().getUuid());
+            boolean isNewPlayer;
+            try {
+                isNewPlayer = !Universe.get().getPlayerStorage().getPlayers().contains(event.getPlayerRef().getUuid());
+            } catch (IOException _) {
+                isNewPlayer = true;
+            }
+
             if (isNewPlayer) {
                 for (String s : config.get().welcomeMessage) {
                     event.getPlayerRef().sendMessage(TinyMsg.parse(s.replace("%player%", event.getPlayerRef().getUsername())));
@@ -60,12 +55,5 @@ public class HeroBase extends JavaPlugin {
                 });
             }
         });
-    }
-
-    @Override
-    protected void shutdown() {
-        super.shutdown();
-
-        TRACKER.syncSave();
     }
 }
